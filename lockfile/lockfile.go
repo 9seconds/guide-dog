ceb13f43b71fa85f009f74a388a77c44001501df
9seconds <nineseconds@yandex.ru>
2015-03-05 21:11:32 +0300

    Add lockfile implementation

---
+++ lockfile/lockfile.go
@@ -0,0 +1,49 @@
+package lockfile
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+type Lock struct {
+	name string
+	file *os.File
+}
+
+func (l *Lock) String() string {
+	return fmt.Sprintf("<Lock=(filename='%s', file='%v')>", l.name, l.file)
+}
+
+func (l *Lock) Acquire() (err error) {
+	if l.file == nil {
+		return
+	}
+
+	file, err := os.Create(l.name)
+	if err != nil {
+		return
+	}
+
+	err = syscall.Flock(file.Fd(), syscall.LOCK_EX|syscall.LOCK_NB)
+	if err != nil {
+		file.Close()
+		return
+	}
+
+	l.file = file
+
+	return
+}
+
+func (l *Lock) Release() error {
+	defer l.file.Close()
+
+	err = syscall.Flock(l.file.Fd(), syscall.LOCK_UN)
+	l.file.Close()
+	l.file = nil
+}
+
+func NewLock(filename string) *Lock {
+	return &Lock{name: filename}
+}
