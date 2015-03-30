4da93a7fac955637e0a087991b45314ebad06e66
9seconds <nineseconds@yandex.ru>
2015-03-30 20:45:28 +0300

    Add unittests for the rest of parsers

--- internal/environment/parsers_test.go
+++ internal/environment/parsers_test.go
@@ -2,6 +2,8 @@ package environment
 
 import (
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
 	assert "github.com/stretchr/testify/assert"
@@ -14,6 +16,11 @@ func getFileNameWithContent(data string) string {
 	file.WriteString(data)
 
 	return file.Name()
+}
+
+func createEnvDirVariable(dir, name, content string) {
+	path := filepath.Join(dir, name)
+	ioutil.WriteFile(path, []byte(content), os.FileMode(0666))
 }
 
 func TestConfigFormatJSONOk(t *testing.T) {
@@ -123,4 +130,89 @@ toot-toot:
 	_, err := configFormatJSONParser(fileName)
 
 	assert.NotNil(t, err)
+}
+
+func TestConfigFormatINIOk(t *testing.T) {
+	const data = `[somesection]
+hello=world
+foo = bar
+
+[section2]
+bar =   baaz`
+
+	fileName := getFileNameWithContent(data)
+	result, err := configFormatINIParser(fileName)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(result), 3)
+	assert.Equal(t, result["hello"], "world")
+	assert.Equal(t, result["foo"], "bar")
+	assert.Equal(t, result["bar"], "baaz")
+}
+
+func TestConfigFormatINIFail(t *testing.T) {
+	const data = `[somesec`
+
+	fileName := getFileNameWithContent(data)
+	_, err := configFormatINIParser(fileName)
+
+	assert.NotNil(t, err)
+}
+
+func TestConfigFormatNoSections(t *testing.T) {
+	const data = "t = 2"
+
+	fileName := getFileNameWithContent(data)
+	result, err := configFormatINIParser(fileName)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(result), 1)
+	assert.Equal(t, result["t"], "2")
+}
+
+func TestConfigFormatEnvDirParserNoDir(t *testing.T) {
+	_, err := configFormatEnvDirParser("WTF")
+
+	assert.NotNil(t, err)
+}
+
+func TestConfigFormatEnvDirParserCorrectFiles(t *testing.T) {
+	tempDir, _ := ioutil.TempDir("", "")
+	defer os.RemoveAll(tempDir)
+
+	createEnvDirVariable(tempDir, "hello", "world")
+	createEnvDirVariable(tempDir, "foo", "bar")
+
+	result, err := configFormatEnvDirParser(tempDir)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(result), 2)
+	assert.Equal(t, result["hello"], "world")
+	assert.Equal(t, result["foo"], "bar")
+}
+
+func TestConfigFormatEnvDirParserEmptyFile(t *testing.T) {
+	tempDir, _ := ioutil.TempDir("", "")
+	defer os.RemoveAll(tempDir)
+
+	createEnvDirVariable(tempDir, "hello", "")
+
+	result, err := configFormatEnvDirParser(tempDir)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(result), 1)
+	assert.Equal(t, result["hello"], "")
+}
+
+func TestConfigFormatEnvDirParserSkipDirs(t *testing.T) {
+	tempDir, _ := ioutil.TempDir("", "")
+	defer os.RemoveAll(tempDir)
+
+	path := filepath.Join(tempDir, "WTF")
+	os.Mkdir(path, os.FileMode(0666))
+
+	result, err := configFormatEnvDirParser(tempDir)
+
+	assert.Nil(t, err)
+	assert.Equal(t, len(result), 0)
 }
